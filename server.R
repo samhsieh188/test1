83bdbe1f56a93190acbec67af61867b2905b98d4
samhsieh188 <samhsieh18866@gmail.com>
2016-07-18 15:14:27 +0800

    ver 1
    ver 1

---
+++ server.R
@@ -0,0 +1,40 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(quantmod)
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R  
+    # 某一段資料放到X
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+  output$testPlot<-renderPlot({
+    
+   getSymbols("AAPL", auto.assign=FALSE)
+   
+    APPL=getSymbols("AAPL", src="yahoo")
+    #barChart(AAPL)
+    #chartSeries(tw2330, theme="white")
+    #chartSeries(AAPL["2016-01::2016-06"],theme="white",TA="addSMA(5);addSMA(20);addCCI(20)")
+    chartSeries(AAPL["2016-01::2016-06"],theme="white")
+  })
+  
+ 
+  
+})
